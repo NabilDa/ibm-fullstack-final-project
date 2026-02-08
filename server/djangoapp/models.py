efa16f891a70bd4f3bb3f4f50639c0825b1811aa
NabilDa <naabil.daoui@gmail.com>
2026-02-08 13:06:14 +0100

    Fix formatting for Python files

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,14 +1,11 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    
+
     def __str__(self):
         return self.name
 
@@ -26,8 +23,9 @@ class CarModel(models.Model):
         validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
-        ]
+            ]
     )
-    
+
     def __str__(self):
         return self.name
+
